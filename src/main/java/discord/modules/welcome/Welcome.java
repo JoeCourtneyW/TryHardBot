b5d427085f6dc767498780249e291b0c40f7156e
Joe Courtney <SlyBro3@gmail.com>
2017-05-21 23:50:15 -0400

    Automatically add new users to roles

--- src/main/java/discord/modules/welcome/Welcome.java
+++ src/main/java/discord/modules/welcome/Welcome.java
@@ -5,12 +5,29 @@ import discord.utils.MessageUtils;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.UserJoinEvent;
 import sx.blah.discord.handle.impl.events.UserLeaveEvent;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
 
 public class Welcome {
 
 	
 	@EventSubscriber
 	public void onJoin(UserJoinEvent e){
+		IUser user = e.getUser();
+		IGuild g = e.getGuild();
+		try {
+			if(g.getID().equalsIgnoreCase("295679840659439638")) {
+				user.addRole(g.getRolesByName("Set Platform").get(0));
+				user.addRole(g.getRolesByName("Set Games").get(0));
+				user.addRole(g.getRolesByName("Set Rank").get(0));
+				user.addRole(g.getRolesByName("Set Region").get(0));
+			}
+		}catch(MissingPermissionsException | RateLimitException | DiscordException en){
+			en.printStackTrace();
+		}
 		String message = ConfigValue.WELCOME_MESSAGE.getValue();
 		message = message.replaceAll("%user%", e.getUser().getName());
 		message = message.replaceAll("%server%", e.getGuild().getName());
